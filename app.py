fde114674c6368ce220217f061abfb63ec4bbca1
ZhichengDai <lifelongcoding@gmail.com>
2023-06-11 23:29:07 +0800

    Complete the streamlit script

--- app.py
+++ app.py
@@ -1,25 +1,167 @@
 from streamlit_webrtc import webrtc_streamer
+import streamlit as st
 import av
 import cv2
 import mediapipe as mp
+import numpy as np
+
+from keras.models import load_model
 
 
-mpHands = mp.solutions.hands
-hands = mpHands.Hands()
-mpDraw = mp.solutions.drawing_utils
+mp_holistic = mp.solutions.holistic  # Holistic model
+holistic = mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5)
+mp_drawing = mp.solutions.drawing_utils  # Drawing utilities
+
+model = load_model('action.h5')
+
+actions = np.array(['hello', 'thanks', 'iloveyou', 'ihateyou'])
+
+# Thirty videos worth of data
+no_sequences = 30
+
+# Videos are going to be 30 frames in length
+sequence_length = 30
+
+# Folder start
+start_folder = 30
+
+colors = [(245, 117, 16), (117, 245, 16), (16, 117, 245), (255, 0, 255)]
+
+sequence = []
+sentence = []
+predictions = []
+threshold = 0.5
+
+
+def mediapipe_detection(image, model):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # COLOR CONVERSION BGR 2 RGB
+    image.flags.writeable = False  # Image is no longer writeable
+    results = model.process(image)  # Make prediction
+    image.flags.writeable = True  # Image is now writeable
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)  # COLOR CONVERSION RGB 2 BGR
+    return image, results
+
+
+def draw_styled_landmarks(image, results):
+    # Draw face connections
+    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_CONTOURS,
+                              mp_drawing.DrawingSpec(color=(80, 110, 10), thickness=1, circle_radius=1),
+                              mp_drawing.DrawingSpec(color=(80, 256, 121), thickness=1, circle_radius=1)
+                              )
+    # Draw pose connections
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
+                              mp_drawing.DrawingSpec(color=(80, 22, 10), thickness=2, circle_radius=4),
+                              mp_drawing.DrawingSpec(color=(80, 44, 121), thickness=2, circle_radius=2)
+                              )
+    # Draw left hand connections
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                              mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4),
+                              mp_drawing.DrawingSpec(color=(121, 44, 250), thickness=2, circle_radius=2)
+                              )
+    # Draw right hand connections
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                              mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=4),
+                              mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
+                              )
+
+
+def extract_keypoints(results):
+    pose = np.array([[res.x, res.y, res.z, res.visibility] for res in
+                     results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(33 * 4)
+    face = np.array([[res.x, res.y, res.z] for res in
+                     results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(468 * 3)
+    lh = np.array([[res.x, res.y, res.z] for res in
+                   results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21 * 3)
+    rh = np.array([[res.x, res.y, res.z] for res in
+                   results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(
+        21 * 3)
+    return np.concatenate([pose, face, lh, rh])
+
+
+def prob_viz(res, actions, input_frame, colors):
+    output_frame = input_frame.copy()
+    for num, prob in enumerate(res):
+        cv2.rectangle(output_frame, (0, 60 + num * 40), (int(prob * 100), 90 + num * 40), colors[num], -1)
+        cv2.putText(output_frame, actions[num], (0, 85 + num * 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2,
+                    cv2.LINE_AA)
+
+    return output_frame
 
 
 def video_frame_callback(frame):
-    img = frame.to_ndarray(format="bgr24")
-    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    result = hands.process(img_rgb)
-    # print(result.multi_hand_landmarks)
-    
-    if result.multi_hand_landmarks:
-        for handLms in result.multi_hand_landmarks:
-            mpDraw.draw_landmarks(img_rgb, handLms, mpHands.HAND_CONNECTIONS)
-            
-    return av.VideoFrame.from_ndarray(img_rgb, format="rgb24")
+    frame = frame.to_ndarray(format="bgr24")
+    image, results = mediapipe_detection(frame, holistic)
+    draw_styled_landmarks(image, results)
+
+    global sequence
+    global sentence
+    global predictions
+    global threshold
+
+    keypoints = extract_keypoints(results)
+    sequence.append(keypoints)
+    sequence = sequence[-30:]
+
+    if len(sequence) == 30:
+        res = model.predict(np.expand_dims(sequence, axis=0))[0]
+        print(actions[np.argmax(res)])
+        predictions.append(np.argmax(res))
+
+        # 3. Viz logic
+        if np.unique(predictions[-10:])[0] == np.argmax(res):
+            if res[np.argmax(res)] > threshold:
+
+                if len(sentence) > 0:
+                    if actions[np.argmax(res)] != sentence[-1]:
+                        sentence.append(actions[np.argmax(res)])
+                else:
+                    sentence.append(actions[np.argmax(res)])
+
+        if len(sentence) > 5:
+            sentence = sentence[-5:]
+
+        # Viz probabilities
+        image = prob_viz(res, actions, image, colors)
+
+    cv2.rectangle(image, (0, 0), (640, 40), (245, 117, 16), -1)
+    cv2.putText(image, ' '.join(sentence), (3, 30),
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+
+    return av.VideoFrame.from_ndarray(image, format="bgr24")
 
 
+st.title("Action Detection Web App")
+st.subheader("This app allows you to play with hand gesture using machine learning!")
+st.text("ML01 Machine Learning, UTSEUS, Shanghai University")
+
+# sidebar experiment
+# Using object notation
+add_selectbox = st.sidebar.selectbox(
+    "Would you like to spend time learning ML?",
+    ("Absolutely yes", "Possibly", "Never")
+)
+# Using "with" notation
+with st.sidebar:
+    add_radio = st.radio(
+        "How do you like our project?",
+        ("Not much", "Just so-so", "Great", "Perfect")
+    )
+
+# Using tabs
+tab1, tab2, tab3, tab4 = st.tabs(["hello", "thanks", "i love you", "i hate you"])
+with tab1:
+    st.header("hello")
+    st.image("example/hello.png", width=500)
+
+with tab2:
+    st.header("thanks")
+    st.image("example/thanks.png", width=500)
+
+with tab3:
+    st.header("i love you")
+    st.image("example/iloveyou.png", width=500)
+
+with tab4:
+    st.header("i hate you")
+    st.image("example/ihateyou.png", width=500)
 webrtc_streamer(key="gesture recognition", video_frame_callback=video_frame_callback)
